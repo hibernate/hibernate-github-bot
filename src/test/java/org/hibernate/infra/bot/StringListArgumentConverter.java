d073dc795637e20bcb18eb4aa776766ab1aa8ab2
Yoann Rodière <yoann@hibernate.org>
2022-03-09 09:17:46 +0100

    Handle commits that mention multiple JIRA issues

---
+++ src/test/java/org/hibernate/infra/bot/StringListArgumentConverter.java
@@ -0,0 +1,30 @@
+package org.hibernate.infra.bot;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+public class StringListArgumentConverter extends SimpleArgumentConverter {
+	private static final String SEPARATOR = ";";
+
+	@Override
+	protected List<String> convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+		if ( source == null ) {
+			return Collections.emptyList();
+		}
+		if ( !( source instanceof String ) ) {
+			throw new ArgumentConversionException( "Can only convert from String" );
+		}
+		if ( targetType != List.class ) {
+			throw new ArgumentConversionException( "Can only convert to List<String>" );
+		}
+		String sourceString = (String) source;
+		if ( sourceString.isEmpty() ) {
+			return Collections.emptyList();
+		}
+		return Arrays.asList( sourceString.split( SEPARATOR ) );
+	}
+}
