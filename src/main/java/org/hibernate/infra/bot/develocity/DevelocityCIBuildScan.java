13cef06b296a3ae752f60c5cfe87709ee5ba90e1
Yoann Rodière <yoann@hibernate.org>
2024-03-21 17:28:48 +0100

    Allow partial extraction of build scans even if some error out

--- src/main/java/org/hibernate/infra/bot/develocity/DevelocityCIBuildScan.java
+++ src/main/java/org/hibernate/infra/bot/develocity/DevelocityCIBuildScan.java
@@ -1,12 +1,20 @@
 package org.hibernate.infra.bot.develocity;
 
 import java.net.URI;
+import java.util.Comparator;
 import java.util.List;
 
 public record DevelocityCIBuildScan(String provider, String jobOrWorkflow, String stage,
 									Long availableAt, List<String> tags, List<String> goals,
 									Status status, Status testStatus,
 									URI buildScan, URI tests, URI logs) {
+
+	public static final Comparator<? super DevelocityCIBuildScan> COMPARATOR =
+			Comparator.comparing( DevelocityCIBuildScan::provider )
+					.thenComparing( DevelocityCIBuildScan::jobOrWorkflow )
+					.thenComparing( DevelocityCIBuildScan::stage )
+					.thenComparing( DevelocityCIBuildScan::availableAt );
+
 	public enum Status {
 		SUCCESS,
 		FAILURE
